3dbad36fd9edef8e0b172f59480ad965fc372a7c
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-05 15:41:48 -0800

    Incorporate ejs partials to views

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,11 +1,17 @@
 const Flight = require('../models/flight');
+const { render } = require('../server');
 
 module.exports = {
-    index
+    index,
+    new: newFlight
 }
 
 function index (req, res) {
     Flight.find({}, function (err, flights) {
-        res.render('flights/index', {flights})
+        res.render('flights/index', {flights, title: 'All Flights'})
     })
+}
+
+function newFlight (req, res) {
+    res.render('flights/new', {title: 'New Flight'})
 }

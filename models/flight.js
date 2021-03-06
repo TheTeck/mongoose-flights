39a236f3beaea921039eeacc529401f58541b8a4
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-06 15:23:58 -0800

    Add destination model schema and include in flight schema as subdocument

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    arrival: Date
+})
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -21,7 +29,8 @@ const flightSchema = new Schema({
         default: function () {
             return new Date(+new Date() + 365*24*60*60*1000)
         }
-    }
+    },
+    destinations: [destinationSchema]
 })
 
 module.exports = mongoose.model('Flight', flightSchema)

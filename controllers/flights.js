56b92415496edc6ba62e18cd54c5b2e699e0c45e
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-05 21:52:44 -0800

    Add default date in new flight input and display flights in ascending order

--- controllers/flights.js
+++ controllers/flights.js
@@ -9,19 +9,22 @@ module.exports = {
 
 function index (req, res) {
     Flight.find({}, function (err, flights) {
+        flights.sort(function (a, b) {
+            return +a.departs - +b.departs
+        })
         res.render('flights/index', {flights, title: 'All Flights'})
     })
 }
 
 function newFlight (req, res) {
-    res.render('flights/new', {title: 'Add Flight'})
+    const flight = new Flight()
+    const dt = flight.departs
+    const departsDate = dt.toISOString().slice(0, 16);
+    res.render('flights/new', {title: 'Add Flight', departsDate})
 }
 
 function create (req, res) {
     const flight = new Flight(req.body)
-    const dt = flight.departs
-    const departsDate = dt.toISOString().slice(0, 16);
-
     flight.save(function(err) {
         if (err) return res.redirect('/flights/new')
         res.redirect('/flights')
--- models/flight.js
+++ models/flight.js
@@ -19,7 +19,7 @@ const flightSchema = new Schema({
     departs: {
         type: Date,
         default: function () {
-            return new Date().getFullYear() + 1;
+            return new Date(+new Date() + 365*24*60*60*1000)
         }
     }
 })
